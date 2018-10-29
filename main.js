908f5fd1340189b9e5f86022143df486a5ff6d1a
RagingHydra <sweetiesellsapples@gmail.com>
2018-10-30 02:51:16 +0300

    added !petuh command

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require("./config_toxic.json");
 
 const bot = new Discord.Client();
 bot.config = config;
 
-bot.on("message", (message) => {
+bot.on("message", message => {
   //Petuh logic
   if (message.content.startsWith("!petuh")) {
-    message.channel.send("Петушара бля!");
+    message.guild.members.forEach(member => {
+      if (member.hoistRole != null) {
+        if (member.hoistRole.name === "@petuh") {
+          message.channel.send(
+            member.user.username + " носит позорный титул петуха!"
+          );
+        }
+      }
+    });
+  }
+  if (message.content.startsWith("!petuhvote")) {
+    //TODO: Vote
   }
 });
 
