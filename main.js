fcc6100d20f65b0d8b94064d7e1b4558099a6710
RagingHydra <sweetiesellsapples@gmail.com>
2018-10-30 01:49:31 +0300

    struggle

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 
 const bot = new Discord.Client();
+bot.config = config;
+
+bot.on("message", (message) => {
+  //Petuh logic
+  if (message.content.startsWith("!petuh")) {
+  }
+});
 
 bot.login(config.token);
