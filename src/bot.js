cfff76ce7f44a06c25d55bdf6a1c9e2830bbeccd
RagingHydra <sweetiesellsapples@gmail.com>
2018-10-29 23:45:00 +0300

    removed junk

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,6 @@
-import Discord from "discord.js";
+// import Discord from "discord.js";
+const Discord = require("discord.js");
 
 const bot = new Discord.Client();
+
+bot.login("NTA2NTQ3MjgxMjE5MDI2OTQ3.DrjvwA.f1IHlUOUomBaANk3DCPGSY5uWII");
