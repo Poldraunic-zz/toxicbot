b8951ec6bd36dde1700bf114f9cd52a538c12f4c
RagingHydra <sweetiesellsapples@gmail.com>
2018-10-30 00:01:48 +0300

    removed token

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,5 @@
-// import Discord from "discord.js";
 const Discord = require("discord.js");
 
 const bot = new Discord.Client();
 
-bot.login("NTA2NTQ3MjgxMjE5MDI2OTQ3.DrjvwA.f1IHlUOUomBaANk3DCPGSY5uWII");
+bot.login(token);
