b5e6f80dc38525339cc0333c5cf1b11b0722c4bc
Poldraunic <krugletsov.egor@gmail.com>
2018-10-30 17:06:35 +0300

    Clean up
    Small clean up to increase code clarity and reduce amount of typing required.

--- main.js
+++ main.js
@@ -2,24 +2,26 @@ const Discord = require("discord.js");
 const config = require("./config_toxic.json");
 
 const bot = new Discord.Client();
-bot.config = config;
 
 bot.on("message", message => {
+  const channel = message.channel;
+  const content = message.content;
+  const guild   = message.guild;
+  const member  = message.member;
+
   //Petuh command logic
-  if (message.content.startsWith("!petuh")) {
-    message.guild.members.forEach(member => {
-      if (member.hoistRole != null) {
-        if (member.hoistRole.name === "@petuh") {
-          message.channel.send(
-            member.user.username + " носит позорный титул петуха!"
-          );
-        }
+  if (content === "!petuh") {
+    guild.members.forEach(m => {
+      if ((!m.hoistRole) && (m.hoistRole.name === "@petuh")) {
+        channel.send(member.user.username + " носит позорный титул петуха!");
       }
     });
   }
-  if (message.content.startsWith("!petuhvote")) {
+
+  if (content.startsWith("!petuhvote ")) {
     //TODO: Vote
   }
+
 });
 
 bot.login(config.token);
