4549182c4517182f826e98f4340d13b5a989fc80
Poldraunic <krugletsov.egor@gmail.com>
2018-10-30 17:12:58 +0300

    Introduce command to add SAR

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const config = require("./config_toxic.json");
 
 const bot = new Discord.Client();
+let roleList = [];
 
 bot.on("message", message => {
   const channel = message.channel;
@@ -20,6 +21,37 @@ bot.on("message", message => {
 
   if (content.startsWith("!petuhvote ")) {
     //TODO: Vote
+  }
+
+  // Allows members with ADMINISTRATOR permission to add roles to the list of
+  // self assignable roles.
+  if (content.startsWith("!asar ") && member.hasPermission("ADMINISTRATOR")) {
+    // Check if role exists
+    const roles = guild.roles;
+    const roleName =  content.slice(6); // FIXME: Get rid of magic number later.
+    const role = roles.find(r => r.name.toLowerCase() === roleName.toLowerCase());
+
+    if (!role) {
+      console.log("No such role exists.");
+      return;
+    }
+
+    // Check if role is in list already.
+    if (roleList.includes(role)) {
+      console.log(`${role.name} is already in the list.`);
+      const embed = new Discord.RichEmbed()
+        .setDescription(`Role **${role.name}** is already in the list.`)
+        .setColor(0xDE2A61);
+      channel.send(embed);
+      return;
+    }
+
+    // Add role name to the list.
+    roleList.push(role.name.toLowerCase());
+    const embed = new Discord.RichEmbed()
+      .setDescription(`Role **${role.name}** has been added to the list.`)
+      .setColor(0x61DE2A);
+    channel.send(embed);
   }
 
 });
